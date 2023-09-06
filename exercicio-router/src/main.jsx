ce3a47354f90a3d4727a489eaab3a32523ae3147
jaciteixeira <rm99627@fiap.com.br>
2023-09-05 21:07:43 -0300

    importado elementos das rotas

--- exercicio-router/src/main.jsx
+++ exercicio-router/src/main.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './routes/home.jsx';
+import Produtos from './routes/Produtos.jsx';
+import EditarProdutos from './routes/EditarProdutos.jsx';
+import Error from './routes/error.jsx';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
