eaa6bfbd6d5df5f5e29432da380ba064a2205de0
jaciteixeira <rm99627@fiap.com.br>
2023-09-05 21:50:15 -0300

    adicionado rodape as paginas

--- exercicio-router/src/App.jsx
+++ exercicio-router/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Menu from './components/Menu'
+import Rodape from './components/Rodape'
 import { Outlet } from 'react-router-dom'
 
 function App() {
@@ -12,6 +13,8 @@ function App() {
     <>
       <Menu></Menu>
       <Outlet/>
+      <Rodape></Rodape>
+
     </>
   )
 }
