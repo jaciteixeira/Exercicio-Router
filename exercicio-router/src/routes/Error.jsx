0330d38eeab5c9b131121fa426cc98c1dd2846f6
jaciteixeira <rm99627@fiap.com.br>
2023-09-05 21:07:00 -0300

    criado paginas de rotas

---
+++ exercicio-router/src/routes/Error.jsx
@@ -0,0 +1,7 @@
+export default function Error(){
+    return(
+        <main>
+            <h1>Erro 404 - Pégina não encontrada</h1>
+        </main>
+    )
+}
---
+++ exercicio-router/src/routes/Produtos.jsx
@@ -0,0 +1,7 @@
+export default function Produtos(){
+    return(
+        <main>
+            <h1>Produtos</h1>
+        </main>
+    )
+}
